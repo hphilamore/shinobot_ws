0ac06003ea11319b1b3cc1a4fb30d9ba2f451379
hphilamore <hemmaphilamore@gmail.com>
2020-07-15 21:42:23 +0100

    tof distance sense moved back to scripts"

---
+++ src/shinobot/scripts/VL53L0X_rasp_python/python/VL53L0X_example.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+
+# MIT License
+# 
+# Copyright (c) 2017 John Bryan Moore
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import time
+import VL53L0X
+
+#############################################
+import RPi.GPIO as GPIO ##
+
+# GPIO for Sensor 1 shutdown pin
+sensor1_shutdown = 20
+# GPIO for Sensor 2 shutdown pin
+sensor2_shutdown = 17#16
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sensor1_shutdown, GPIO.OUT)
+GPIO.setup(sensor2_shutdown, GPIO.OUT)
+# Set all shutdown pins low to turn off each VL53L0X
+GPIO.output(sensor1_shutdown, GPIO.LOW)
+GPIO.output(sensor2_shutdown, GPIO.LOW)
+
+# Keep all low for 500 ms or so to make sure they reset
+time.sleep(0.50)
+
+GPIO.output(sensor1_shutdown, GPIO.HIGH)
+
+#############################################
+
+
+# Create a VL53L0X object
+tof = VL53L0X.VL53L0X(i2c_bus=1,i2c_address=0x29)
+#tof = VL53L0X.VL53L0X(i2c_bus=1,i2c_address=0x2B)
+# I2C Address can change before tof.open()
+#tof.change_address(0x32)
+#tof.change_address(0x2B)
+tof.open()
+# Start ranging
+tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+
+timing = tof.get_timing()
+if timing < 20000:
+    timing = 20000
+print("Timing %d ms" % (timing/1000))
+
+tof.close()
+
+
+
+
+
+tof.open()
+# Start ranging
+tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+
+for count in range(1, 101):
+    distance = tof.get_distance()
+    if distance > 0:
+        print("%d mm, %d cm, %d" % (distance, (distance/10), count))
+
+    time.sleep(timing/1000000.00)
+
+tof.stop_ranging()
+tof.close()
+
+
+GPIO.output(sensor1_shutdown, GPIO.LOW)
+# Keep all low for 500 ms or so to make sure they reset
+time.sleep(0.50)
+
+
+GPIO.output(sensor2_shutdown, GPIO.HIGH)
+
+#############################################
+
+
+# Create a VL53L0X object
+#tof = VL53L0X.VL53L0X(i2c_bus=1,i2c_address=0x29)
+#tof = VL53L0X.VL53L0X(i2c_bus=1,i2c_address=0x2B)
+# I2C Address can change before tof.open()
+#tof.change_address(0x32)
+#tof.change_address(0x2B)
+tof.open()
+# Start ranging
+tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+
+# timing = tof.get_timing()
+# if timing < 20000:
+#     timing = 20000
+# print("Timing %d ms" % (timing/1000))
+
+for count in range(1, 101):
+    distance = tof.get_distance()
+    if distance > 0:
+        print("%d mm, %d cm, %d" % (distance, (distance/10), count))
+
+    time.sleep(timing/1000000.00)
+
+tof.stop_ranging()
+tof.close()
+
---
+++ src/shinobot/scripts/VL53L0X_rasp_python/python/VL53L0X_multi.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+
+# MIT License
+# 
+# Copyright (c) 2017 John Bryan Moore
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import time
+import VL53L0X
+import RPi.GPIO as GPIO ##
+
+# 'L', 'C', 'R'
+shut_pins = [20, 17, 16]
+timing = 0.0
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+for pin in shut_pins:
+	GPIO.setup(pin, GPIO.OUT)
+	GPIO.output(pin, GPIO.LOW)
+
+# Keep all low for 500 ms or so to make sure they reset
+time.sleep(0.50)
+
+# Create a VL53L0X object
+tof = VL53L0X.VL53L0X(i2c_bus=1,i2c_address=0x29)
+
+def get_timing():
+	""" 
+	Sets up timing used to measure distance 
+	"""
+	GPIO.output(shut_pins[0], GPIO.HIGH)
+
+	tof.open()
+	# Start ranging
+	tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+
+	timing = tof.get_timing()
+	if timing < 20000:
+	    timing = 20000
+	print("Timing %d ms" % (timing/1000))
+
+	tof.close()
+
+	GPIO.output(shut_pins[0], GPIO.LOW)
+
+	# Keep all low for 500 ms or so to make sure they reset
+	time.sleep(0.50)
+
+def distance_sense():
+	"""
+	Measure distance each sensor
+	"""
+	distance_array = []
+
+	for pin in shut_pins:
+		GPIO.output(pin, GPIO.HIGH)
+					# Keep all low for 500 ms or so to make sure they reset
+		#time.sleep(0.50)
+
+		tof.open()
+		# Start ranging
+		tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+		distance = tof.get_distance()
+		distance_array.append(distance)
+
+	    # if distance > 0:
+	    #     print("%d mm, %d cm, %d" % (distance, (distance/10), count))
+		#time.sleep(timing/1000000.00)
+
+		tof.stop_ranging()
+		tof.close()
+
+		GPIO.output(pin, GPIO.LOW)
+		# Keep all low for 500 ms or so to make sure they reset
+		time.sleep(0.50)
+
+	return distance_array
+
+
+get_timing()
+
+#for pin in shut_pins:
+for count in range(1, 101):
+	D = distance_sense()
+
+	print(D)
