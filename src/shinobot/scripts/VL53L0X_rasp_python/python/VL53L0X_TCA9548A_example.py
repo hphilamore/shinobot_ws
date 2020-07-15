750241d33641e321bd0bfa9083dbacec3b3c443b
hphilamore <hemmaphilamore@gmail.com>
2020-07-15 11:47:45 +0100

    3 LIDAR sensors

--- src/shinobot/scripts/VL53L0X_rasp_python/python/VL53L0X_TCA9548A_example.py
+++ src/shinobot/scripts/VL53L0X_rasp_python/python/VL53L0X_TCA9548A_example.py
@@ -29,13 +29,16 @@ import VL53L0X
 tof1 = VL53L0X.VL53L0X(tca9548a_num=1, tca9548a_addr=0x70)
 # Create a VL53L0X object for device on TCA9548A bus 2
 tof2 = VL53L0X.VL53L0X(tca9548a_num=2, tca9548a_addr=0x70)
+tof3 = VL53L0X.VL53L0X(tca9548a_num=2, tca9548a_addr=0x70)
 tof1.open()
 tof2.open()
+tof3.open()
 
 # Start ranging on TCA9548A bus 1
 tof1.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
 # Start ranging on TCA9548A bus 2
 tof2.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+tof3.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
 
 timing = tof1.get_timing()
 if timing < 20000:
@@ -53,11 +56,20 @@ for count in range(1, 101):
     if distance > 0:
         print("2: %d mm, %d cm, %d" % (distance, (distance/10), count))
 
-    print()
+
+    # Get distance from VL53L0X  on TCA9548A bus 2
+    distance = tof3.get_distance()
+    if distance > 0:
+        print("3: %d mm, %d cm, %d" % (distance, (distance/10), count))
+
+
+    print("  ")
     time.sleep(timing/1000000.00)
 
 tof1.stop_ranging()
 tof2.stop_ranging()
+tof2.stop_ranging()
 
 tof1.close()
 tof2.close()
+tof3.close()
